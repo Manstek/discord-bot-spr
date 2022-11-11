396085f005c9aeccf21a3b640fb59b554e3c57ba
Artyom <aguczalov@mail.ru>
2022-11-11 18:18:11 +0300

    Create first command /ping !!!

--- discord bot/index.js
+++ discord bot/index.js
@@ -4,7 +4,7 @@ const { token } = require('./config.json');
 const client=new Client({
     Intents:[
         Intents.FLAGS.GUILDS,//adds server functionality
-        Intents.FLAGS.GUILD_MESSAGES //gets messages from our bot.
+        Intents.FLAGS.GUILD_MESSAGES, //gets messages from our bot.
     ]
 });
 
@@ -12,12 +12,27 @@ client.once("ready", () =>{
     console.log(client.user.username + " is ONLINE"); //message when bot is online
 })
 
+const prefix = "/";
 
-client.on('message',
-function (messages){
-    if(messages.content.toLocaleLowerCase()==='hello') {
-        messages.channel.send('hello' + ' '  + messages.author.username); //reply hello word message with senders name
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        console.log(client.user.username + " see yor command!");
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
     }
-})
+  });
+
+// client.on('message', function (messages){
+//     if(messages.content.toLocaleLowerCase()==='hello') {
+//         messages.channel.send('hello' + ' '  + messages.author.username); //reply hello word message with senders name
+//     }
+// })
 
 client.login(token);
